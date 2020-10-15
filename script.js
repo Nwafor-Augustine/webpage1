236861c854e836a94440ea6d3153d472d41f6e81
nwaforaugustine <nwaforaugustine411@gmail.co>
2020-10-15 14:07:33 +0100

    update

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 
+var checkscreen1 = window.matchMedia("(max-width:480px)");
+if (checkscreen1.matches) {
+    var navdisplay = document.getElementById("aside");
+    var labelshow = document.getElementById("cancel");
+    var bar = document.getElementById("bar");
+    var menucheck = document.getElementById("menunav");
+    menucheck.addEventListener('change', function () {
+        if (menucheck.checked) {
+            bar.style.left = "-2250px";
+            navdisplay.style.left = "10px";
+            labelshow.style.display = "block"
+        } else {
+            navdisplay.style.left = "-250px";
+            bar.style.left = "85%";
+            labelshow.style.display = "none";
+        }
+    })
+}
 
 
-var navdisplay = document.getElementById("aside");
-var labelshow = document.getElementById("cancel");
-var bar = document.getElementById("bar");
-var menucheck = document.getElementById("menunav");
-menucheck.addEventListener('change', function () {
-    if (menucheck.checked) {
-        bar.style.left = "-2250px";
-        navdisplay.style.left = "10px";
-        labelshow.style.display = "block"
-    } else {
-        navdisplay.style.left = "-250px";
-        bar.style.left = "85%";
-        labelshow.style.display = "none";
-    }
-})
+
 
 
 
